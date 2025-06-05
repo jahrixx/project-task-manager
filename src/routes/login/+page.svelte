60337e68c27560d5b81f5e30d5c7612441287ced
jahrixx <166481107+jahrixx@users.noreply.github.com>
2025-06-05 15:39:21 +0800

    fixed filter labels due to bugs in the login link on the importing of css file globally

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -31,24 +31,24 @@
     }
 </script>
 <title>Login</title>
-<!-- <head>
+<head>
     <link rel="stylesheet" href="src/components/assets/css/login.css">
-</head> -->
+</head>
 
 <ToastContainer />
 <main class="login-container">
     <div class="login-wrapper { loading ? 'loading-state' : '' }">
         <form on:submit={handleLogin}>
             <div class="login-box">
-                <div class="logo-container">
-                    <img src="/logo.png" alt="Logo">
-                    <h2>Project Task Manager</h2>
+                <div class="login-logo-container">
+                    <img class="login-logo" src="/logo.png" alt="Logo">
+                    <h2 class="login-title">Project Task Manager</h2>
                 </div>
-                <input type="text" id="username" placeholder="Username" bind:value={username} required/>
-                <input type="password" id="password" placeholder="Password" bind:value={password} required/>
-                <button type="submit" disabled={loading}>
+                <input class="login-inputs" type="text" id="username" placeholder="Username" bind:value={username} required/>
+                <input class="login-inputs" type="password" id="password" placeholder="Password" bind:value={password} required/>
+                <button class="login-submission" disabled={loading}>
                     {#if loading}
-                        <span class="spinner"></span> Logging In...
+                        <span class="login-spinner"></span> Logging In...
                     {:else}
                         Login
                     {/if}
@@ -57,9 +57,98 @@
         </form>
     </div>
     {#if loading}
-        <div class="fullscreen-loader">
-            <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div class="login-fullscreen-loader">
+            <div class="login-lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
             <!-- <div class="spinner-fullscreen"></div> -->
         </div>
     {/if}
 </main>
+<style>
+    /* Animated FullScreen Loading Spinner */
+    .login-lds-default,
+    .login-lds-default div {
+    box-sizing: border-box;
+    }
+    .login-lds-default {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+    }
+    .login-lds-default div {
+    position: absolute;
+    width: 6.4px;
+    height: 6.4px;
+    background: currentColor;
+    border-radius: 50%;
+    animation: lds-default 1.2s linear infinite;
+    }
+    .login-lds-default div:nth-child(1) {
+    animation-delay: 0s;
+    top: 36.8px;
+    left: 66.24px;
+    }
+    .login-lds-default div:nth-child(2) {
+    animation-delay: -0.1s;
+    top: 22.08px;
+    left: 62.29579px;
+    }
+    .login-lds-default div:nth-child(3) {
+    animation-delay: -0.2s;
+    top: 11.30421px;
+    left: 51.52px;
+    }
+    .login-lds-default div:nth-child(4) {
+    animation-delay: -0.3s;
+    top: 7.36px;
+    left: 36.8px;
+    }
+    .login-lds-default div:nth-child(5) {
+    animation-delay: -0.4s;
+    top: 11.30421px;
+    left: 22.08px;
+    }
+    .login-lds-default div:nth-child(6) {
+    animation-delay: -0.5s;
+    top: 22.08px;
+    left: 11.30421px;
+    }
+    .login-lds-default div:nth-child(7) {
+    animation-delay: -0.6s;
+    top: 36.8px;
+    left: 7.36px;
+    }
+    .login-lds-default div:nth-child(8) {
+    animation-delay: -0.7s;
+    top: 51.52px;
+    left: 11.30421px;
+    }
+    .login-lds-default div:nth-child(9) {
+    animation-delay: -0.8s;
+    top: 62.29579px;
+    left: 22.08px;
+    }
+    .login-lds-default div:nth-child(10) {
+    animation-delay: -0.9s;
+    top: 66.24px;
+    left: 36.8px;
+    }
+    .login-lds-default div:nth-child(11) {
+    animation-delay: -1s;
+    top: 62.29579px;
+    left: 51.52px;
+    }
+    .login-lds-default div:nth-child(12) {
+    animation-delay: -1.1s;
+    top: 51.52px;
+    left: 62.29579px;
+    }
+    @keyframes lds-default {
+    0%, 20%, 80%, 100% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.5);
+    }
+    }
+</style>
